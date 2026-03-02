261a4bf9ead05b6def03f3fac1eb77029e2330b5
kou-sato-ds <yoshirin1989k@gmail.com>
2026-03-02 22:06:52 +0900

    Fix: Update main.tf with additional s3_training_data module

--- 03_Infrastructure/kaggle_project/main.tf
+++ 03_Infrastructure/kaggle_project/main.tf
@@ -1,19 +1,23 @@
-# --- 03_Infrastructure/kaggle_project/main.tf ---
-
 # 1. AWSプロバイダーの設定
 provider "aws" {
-  region = "ap-northeast-1" # 東京リージョン
+  region = "ap-northeast-1" 
 }
 
-# 2. 部品庫（modules）からS3作成機能を呼び出す
+# 2. メインのデータ用バケット
 module "kaggle_s3" {
-  # パスとバケット名をプロ仕様に修正
   source      = "../../AWS_IaC_Terraform/modules/s3_bucket" 
-  bucket_name = "kosato-heart-disease-v9-data" # アンダースコアをハイフンに統一
+  bucket_name = "kosato-heart-disease-v9-data" 
   environment = "dev"
 }
 
-# 3. アウトプット（作成されたバケットIDを画面に表示）
-output "bucket_id" {
+# 3. 学習用データ保管バケット（名前をユニークに！）
+module "s3_training_data" {
+  source      = "../../AWS_IaC_Terraform/modules/s3_bucket"
+  bucket_name = "sato-ds-project-training-data-2026"
+  environment = "dev"
+}
+
+# 4. アウトプット（作成されたバケットIDを表示）
+output "main_bucket_id" {
   value = module.kaggle_s3.s3_bucket_id
 }
