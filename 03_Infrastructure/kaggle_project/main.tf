f1eda4de6d40cc5088601a73a3076849f6893235
kou-sato-ds <yoshirin1989k@gmail.com>
2026-03-04 22:32:13 +0900

    Refactor: Modularize project tags using Terraform variables

--- 03_Infrastructure/kaggle_project/main.tf
+++ 03_Infrastructure/kaggle_project/main.tf
@@ -3,23 +3,28 @@ provider "aws" {
   region = "ap-northeast-1" 
 }
 
-# 2. メインのデータ用バケット
-module "kaggle_s3" {
-  source      = "../../AWS_IaC_Terraform/modules/s3_bucket" 
-  bucket_name = "kosato-heart-disease-v9-data" 
-  environment = "dev"
-  tags        = { Project = "HeartDisease", Owner = "Sato" } # ここに追加
+# 2. 変数の定義（一箇所にまとめる）
+variable "project_tag" {
+  default = "HeartDisease"
 }
 
-# 3. 学習用データ保管バケット
+# 3. メインのデータ用バケット
+module "kaggle_s3" {
+  source      = "../../AWS_IaC_Terraform/modules/s3_bucket"
+  bucket_name = "kosato-heart-disease-v9-data"
+  environment = "dev"
+  tags        = { Project = var.project_tag, Owner = "Sato" } # 変数を使用！
+}
+
+# 4. 学習用データ保管バケット
 module "s3_training_data" {
   source      = "../../AWS_IaC_Terraform/modules/s3_bucket"
   bucket_name = "sato-ds-project-training-data-2026"
   environment = "dev"
-  tags        = { Project = "HeartDisease", Owner = "Sato" } # ここにも追加
+  tags        = { Project = var.project_tag, Owner = "Sato" } # こちらも変数へ！
 }
 
-# 4. アウトプット
+# 5. アウトプット
 output "main_bucket_id" {
   value = module.kaggle_s3.s3_bucket_id
 }
